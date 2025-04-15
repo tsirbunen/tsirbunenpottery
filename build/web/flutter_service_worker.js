fa492aa8e03882885939e9d1940c68c602cfbbb4
Satu Hyvärinen <tsirbunen@gmail.com>
2025-04-15 07:31:24 +0300

    Revert back to no underline

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "571993a8410ab5ea66abd7e6682cca6e",
+const RESOURCES = {"flutter_bootstrap.js": "c5e73b93122ad8edd16d6a2fdff3e961",
 "version.json": "75c58fd5a623ea779525d34368108795",
 "index.html": "7c8d5b02d9c5d220fca308dbc0dfb222",
 "/": "7c8d5b02d9c5d220fca308dbc0dfb222",
-"main.dart.js": "31e99b0764f9b8fce0c93f16ca226dfc",
+"main.dart.js": "1b01073a09b44ac92678664a77c2342e",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
